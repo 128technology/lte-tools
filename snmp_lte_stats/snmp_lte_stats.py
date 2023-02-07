887a18d24ab3c9c996229c1edee2157b28dd1ff1
Mathias Jeschke <mjeschke@juniper.net>
2023-02-07 12:21:18 +0100

    Added file-based caching for API token

--- snmp_lte_stats/snmp_lte_stats.py
+++ snmp_lte_stats/snmp_lte_stats.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import os
+import pathlib
 import requests
 import sys
 import time
@@ -32,6 +33,89 @@ TYPES = {
 NETWORK_TYPES = ('none', 'gsm', 'umts', 'lte')
 
 
+class UnauthorizedException(Exception):
+    pass
+
+class RestGraphqlApi(object):
+    """Representation of REST connection."""
+
+    token = None
+    authorized = False
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    def __init__(self, host='localhost', verify=False, user='admin', password=None, app=__file__):
+        self.host = host
+        self.verify = verify
+        self.user = user
+        self.password = password
+        basename = os.path.basename(app).split('.')[0]
+        self.user_agent = basename
+        self.token_file = os.path.join(
+             pathlib.Path.home(), '.{}.token'.format(basename))
+        self.read_token()
+        self.headers.update({
+             'User-Agent': self.user_agent,
+             'Authorization': f'Bearer {self.token}',
+        })
+
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
+        self.session.hooks['response'].append(self.refresh_token)
+
+    def read_token(self):
+        try:
+            with open(self.token_file) as fd:
+                self.token = fd.read()
+        except FileNotFoundError:
+            pass
+
+    def write_token(self):
+        try:
+            with open(self.token_file, 'w') as fd:
+                fd.write(self.token)
+        except:
+            raise
+
+    def refresh_token(self, r, *args, **kwargs):
+        if r.status_code == 401:
+            token = self.login()
+            self.session.headers.update({'Authorization': f'Bearer {token}'})
+            r.request.headers['Authorization'] = self.session.headers['Authorization']
+            return self.session.send(r.request, verify=self.verify)
+
+    def post(self, location, json, authorization_required=True):
+        """Send data per REST API via post."""
+        url = 'https://{}/api/v1/{}'.format(self.host, location.strip('/'))
+        request = self.session.post(url, json=json, verify=self.verify)
+        return request
+
+    def login(self):
+        json = {
+            'username': self.user,
+        }
+        if self.password:
+            json['password'] = self.password
+        else:
+            key_file = 'pdc_ssh_key'
+            if not os.path.isfile(key_file):
+                key_file = '/home/admin/.ssh/pdc_ssh_key'
+
+            key_content = ''
+            with open(key_file) as fd:
+                key_content = fd.read()
+            json['local'] = key_content
+        request = self.post('/login', json, authorization_required=False)
+        if request.status_code == 200:
+            self.token = request.json()['token']
+            self.write_token()
+            return self.token
+        else:
+            message = request.json()['message']
+            raise UnauthorizedException(message)
+
+
 def parse_arguments():
     """Get commandline arguments."""
     parser = argparse.ArgumentParser(
@@ -45,35 +129,6 @@ def parse_arguments():
 def error(*msg):
     print(*msg)
     sys.exit(1)
-
-
-def post(location, json, token='', host='localhost'):
-    url = 'https://{}/{}'.format(host, location.strip('/'))
-    headers = {
-        'Content-Type': 'application/json',
-    }
-    if token:
-        headers['Authorization'] = 'Bearer {}'.format(token)
-    request = requests.post(
-        url, headers=headers, json=json,
-        verify=False)
-    return request
-
-
-def login(user='admin'):
-    key_content = ''
-    with open('/home/admin/.ssh/pdc_ssh_key') as fd:
-        key_content = fd.read()
-    json = {
-        'username': user,
-        'local': key_content
-    }
-    request = post('/api/v1/login', json)
-    if request.status_code == 200:
-        token = request.json()['token']
-        return token
-    else:
-        error('Login has failed.')
 
 
 def read_cache():
@@ -97,11 +152,11 @@ def write_cache(lte_stats):
         json.dump(lte_stats, fd)
 
 
-def lte_stats(token, interface='LTE1'):
+def lte_stats(api, interface='LTE1'):
     json = {
         'query': '{ allRouters { nodes { nodes { nodes { deviceInterfaces(name: "' + interface + '") { nodes { networkInterfaces { nodes { deviceInterface { state { networkPluginState } } } } } } } } } } }'
     }
-    request = post('/api/v1/graphql', json, token)
+    request = api.post('/graphql', json)
     if request.status_code == 200:
         return request.json()['data']['allRouters']['nodes'][0]['nodes']['nodes'][0]['deviceInterfaces']['nodes'][0]['networkInterfaces']['nodes'][0]['deviceInterface']['state']['networkPluginState']['LTE']
     else:
@@ -118,8 +173,8 @@ def generate_object(index, key, value, type='string'):
 def populate_objects():
     stats = read_cache()
     if not stats:
-        token = login()
-        stats = lte_stats(token)
+        api = RestGraphqlApi()
+        stats = lte_stats(api)
         write_cache(stats)
     objects = {}
     i = 1
